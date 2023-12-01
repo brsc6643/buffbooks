9ccf2476c67ee81929254958f3dc01bca3132a2b
brsc6643 <112126851+brsc6643@users.noreply.github.com>
2023-12-01 14:55:28 -0700

    late revisions

---
+++ buffBooksTest/buffBooksTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  buffBooksTest
+//
+//  Created by ATLAS Laptop 4 Guest on 12/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ buffBooksTest/buffBooksTest/buffBooksTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  buffBooksTestApp.swift
+//  buffBooksTest
+//
+//  Created by ATLAS Laptop 4 Guest on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct buffBooksTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
